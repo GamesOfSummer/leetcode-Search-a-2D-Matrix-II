ec4037a612862c67c93c636522838bb7a8fcb4b3
Summer T <summer@summertime-development.com>
2023-05-01 13:53:17 -0500

    progress

--- src/index.ts
+++ src/index.ts
@@ -5,78 +5,54 @@ import {
     validateFxn,
 } from './helpers.js';
 
-function generateMatrix(arraySize: number): any {
-    if (arraySize < 1) {
-        return [[]];
-    } else if (arraySize == 1) {
-        return [[1]];
+function search(
+    array: number[][],
+    target: number,
+    column: number,
+    row: number
+) {
+    if (column < 0 || row < 0) {
+        return false;
+    } else if (array[column][row] === target) {
+        return true;
     }
 
-    let startColumn = 0;
-    let startRow = 0;
+    let middle = Math.floor(array.length / 2);
+    let midPointRow = array[middle][middle];
 
-    let endcapColumn = arraySize - 1;
-    let endcapRow = arraySize - 1;
+    if (midPointRow < target) {
+        return search(array, target, column - 1, row);
+    } else {
+        return search(array, target, column, row - 1);
+    }
+}
 
-    let counter = 1;
-
-    //populate the array
-    let endResult: number[][] = [];
-
-    for (var i = 0; i < arraySize; i++) {
-        endResult[i] = new Array(arraySize);
+function searchMatrix(array: number[][], target: number): boolean {
+    if (array.length < 1) {
+        return false;
     }
 
-    while (counter <= arraySize * arraySize) {
-        // left to right on row
-        for (let i = startColumn; i <= endcapColumn; i++) {
-            endResult[startColumn][i] = counter;
-            counter++;
-        }
+    let middle = Math.floor(array.length / 2);
+    let midPointRow = array[middle][middle];
 
-        startRow++;
-        // up to down on column
-        for (let i = startRow; i <= endcapRow; i++) {
-            endResult[i][endcapColumn] = counter;
-            counter++;
-        }
-
-        endcapColumn--;
-        // right to left on row
-        for (let i = endcapColumn; i >= startColumn; i--) {
-            endResult[endcapRow][i] = counter;
-            counter++;
-        }
-
-        endcapRow--;
-
-        // down to up on column
-        for (let i = endcapRow; i >= startRow; i--) {
-            endResult[i][startColumn] = counter;
-            counter++;
-        }
-
-        startColumn++;
-    }
-
-    return endResult;
+    return search(array, target, middle, middle);
 }
 
 consoleStart();
 
-validateFxn(generateMatrix(3), [
-    [1, 2, 3],
-    [8, 9, 4],
-    [7, 6, 5],
-]);
-
-validateFxn(generateMatrix(4), [
-    [1, 2, 3],
-    [8, 9, 4],
-    [7, 6, 5],
-]);
-
-validateFxn(generateMatrix(1), [[1]]);
+validateFxn(
+    searchMatrix(
+        [
+            [1, 4, 7, 11, 15],
+            [2, 5, 8, 12, 19],
+            [3, 6, 9, 16, 22],
+            [10, 13, 14, 17, 24],
+            [18, 21, 23, 26, 30],
+        ],
+        5
+    ),
+    true
+);
 
 consoleEnd();
 
